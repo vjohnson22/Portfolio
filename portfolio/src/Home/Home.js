33792ce56bbe633bb8175dbf4c5033faf60d19f1
Victor Johnson <vjohnson22@gmail.com>
2019-09-25 15:54:56 -0400

    updated experienced with on homepage

--- portfolio/src/Home/Home.js
+++ portfolio/src/Home/Home.js
@@ -13,12 +13,16 @@ class Home extends React.Component{
                         <h1><span >Victor Johnson</span></h1>
                         <li>Washington, DC based Web Developer</li> 
                         <li>8 years experience in Professional Services and Customer Success at analytical software startups</li>
+                        
                         </div>
                     </div>
                     <div>
                         <img className='image' src="https://i.imgur.com/6uhwDfR.png"/>
                     </div>
                 </div> 
+                <div className='experience'>
+                    <h2 >Experience with: HTML | CSS | Javascript | React.js | Node.js | MongoDB | Express.js | Django | Python</h2>
+                </div>
                 <div className='buttonSpacing'>   
                     <Link to='/portfolio'><button>Portfolio</button></Link>
                     <Link to='/about'><button>About Me</button></Link>
