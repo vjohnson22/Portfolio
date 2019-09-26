d4c027069911d9574b61616bad0ac294d3c14c79
Victor Johnson <vjohnson22@gmail.com>
2019-09-26 11:11:27 -0400

    updated font and typist to about me

--- portfolio/src/About/About.js
+++ portfolio/src/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import Typist from 'react-typist'
 
 class About extends React.Component{
 
@@ -7,10 +8,12 @@ class About extends React.Component{
         return (
             <div className='body'>
                 <img className='image' src='https://i.imgur.com/29Tuw8A.jpg?1'/>
-                <div>
-                <p>Prior to becoming a Software Engineer, I spent the last 8 years in the SaaS industry, in the analytics space. I started as a business analyst and received several promotions in the Professional Services, and Customer Success organizations.</p> 
-                <p>I hope to take my extensive experience helping corporate clients get value out of our platforms, and use that to create customer friendly products that bring value to organizations </p>
-                </div>
+                <Typist>
+                    <div>
+                        <p>Prior to becoming a Software Engineer, I spent the last 8 years in the SaaS industry, in the analytics space. I started as a business analyst and received several promotions in the Professional Services, and Customer Success organizations.</p> 
+                        <p>I hope to take my extensive experience helping corporate clients get value out of our platforms, and use that to create customer friendly products that bring value to organizations </p>
+                    </div>
+                </Typist>
             </div>
         )
     }
